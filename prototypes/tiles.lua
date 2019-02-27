d79130ee7aa297ae3cfd3dc10184cded7f7d1eb5
jiteman <jiteman@gmail.com>
2019-02-27 11:51:53 +0000

    initial commit 1.0.0

---
+++ prototypes/tiles.lua
@@ -0,0 +1,4 @@
+data.raw["tile"]["concrete"].minable = {hardness = 0, mining_time = 0.1, result = "concrete"}
+data.raw["tile"]["stone-path"].minable = {hardness = 0, mining_time = 0.1, result = "stone-brick"}
+data.raw["tile"]["hazard-concrete-left"].minable = {hardness = 0, mining_time = 0.1, result = "hazard-concrete"}
+data.raw["tile"]["hazard-concrete-right"].minable = {hardness = 0, mining_time = 0.1, result = "hazard-concrete"}
